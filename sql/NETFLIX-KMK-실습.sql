717fa8222bb521a8a7cb541376342072b8a3ff1e
minkyong00 <minkyong131@gmail.com>
2025-07-08 22:26:18 +0900

    NETFLIX-KMK-실습 5~7번 문제 풀이 추가

--- sql/NETFLIX-KMK-실습.sql
+++ sql/NETFLIX-KMK-실습.sql
@@ -30,9 +30,36 @@ GROUP BY TO_CHAR(TO_DATE(DATE_ADDED), 'YYYY')
 ORDER BY RELEASE_YEAR DESC;
 
 -- 5) 두명 이상이 감독으로 참여한 작품의 감독명과 타이틀을 조회
+SELECT DIRECTOR, TITLE
+FROM NETFLIX
+WHERE INSTR(DIRECTOR, ',') > 0;
 
 -- 6) 영화 상영시간에 대해 내림차순 정렬하여 타이틀과 상영시간 조회
+SELECT TITLE, DURATION
+FROM NETFLIX
+WHERE INSTR(DURATION, 'min')>0 -- min 포함
+	AND INSTR(DURATION, 'Seasons') = 0 -- Seasons 제외
+ORDER BY TO_NUMBER(SUBSTR(DURATION, 1, INSTR(DURATION, 'min')-1)) DESC;
+-- DURATION 숫자만 추출해서 숫자로 변환 후 내림차순 정렬함
 
 -- 7) 영화 상영시간이 90분을 넘는 영화와 90분 이하인 영화의 수를 아래와 같이 조회
 --     90분 이하    1,400
 --     90분 초과    2,531
+SELECT '90분 초과', COUNT(*)
+FROM NETFLIX
+WHERE TO_NUMBER(SUBSTR(DURATION, 1, INSTR(DURATION, 'min')-1))>90
+UNION
+SELECT '90분 이하', COUNT(*)
+FROM NETFLIX
+WHERE TO_NUMBER(SUBSTR(DURATION, 1, INSTR(DURATION, 'min')-1))<=90;
+
+
+
+
+
+
+
+
+
+
+
